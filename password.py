960b6bf1c8acc14de78037e8cd2904c5e7bf8fb2
ericyoun430 <eyoun@ucsd.edu>
2021-05-30 21:04:40 -0700

    true

--- password.py
+++ password.py
@@ -1,19 +1,29 @@
 import time
+import hashlib
 
 
-
-
-
+def converter(text):
+    digest = hashlib.sha1(text.encode()).hexdigest()
+    return digest
 
 def main():
     start = time.time()
-    for i in range(1000000):
-        x = 3
+
+    user_input = input("Enter sha1: ")
+    clean_input = user_input.strip().lower()
+    
+    with open('./dictionary.txt') as f:
+        for line in f:
+            test_pass = line.strip()
+            converted_val = converter(test_pass)
+            if (converted_val == clean_input):
+                end = time.time()
+                print("Password has been found in " + str(end-start) + " seconds: " + line)
+                return
+        print("Password not found")
+
     end = time.time()
     print("Runtime is:" + str(end-start))
-
-
-
 
 if __name__ == "__main__":
     main()
